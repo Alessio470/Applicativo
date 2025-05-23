e63a4b9fa368c383992842262b45b9b18107ce84
Alessio470 <nazzaroalessio@gmail.com>
2025-05-23 11:03:30 +0200

    Login

--- src/main/java/gui/Home.java
+++ src/main/java/gui/Home.java
@@ -1,4 +1,33 @@
 package gui;
 
+import javax.swing.*;
+
 public class Home {
+    private JPanel Login;
+    private JLabel textLogin;
+    private JLabel textNome;
+    private JLabel textPassword;
+    private JTextField FieldNome;
+    private JPasswordField FieldPassword;
+
+    private static JFrame frame;
+
+    public Home(){
+        textLogin.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 30));
+        textNome.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 30));
+        textPassword.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 30));
+        FieldNome.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 30));
+        FieldPassword.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 30));
+    }
+
+    public static void main(String[] args) {
+        frame = new JFrame("Home");
+        frame.setContentPane(new Home().Login);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+    }
+
+
 }
