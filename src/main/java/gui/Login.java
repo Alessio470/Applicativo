622fae5e4bb446ee338ba7643570e5486d188a41
Alessio470 <nazzaroalessio@gmail.com>
2025-05-23 11:23:42 +0200

    Login

---
+++ src/main/java/gui/Login.java
@@ -0,0 +1,33 @@
+package gui;
+
+import javax.swing.*;
+
+public class Login {
+    private JPanel Login;
+    private JLabel textLogin;
+    private JLabel textNome;
+    private JLabel textPassword;
+    private JTextField FieldNome;
+    private JPasswordField FieldPassword;
+
+    private static JFrame frame;
+
+    public Login(){
+        textLogin.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 30));
+        textNome.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 30));
+        textPassword.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 30));
+        FieldNome.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 30));
+        FieldPassword.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 30));
+    }
+
+    public static void main(String[] args) {
+        frame = new JFrame("Home");
+        frame.setContentPane(new Login().Login);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+    }
+
+
+}
