1866f7b0c0d0bdb07798a9cd9fe4020ff6401650
M1rio2004 <mario04picone@gmail.com>
2025-05-23 12:15:09 +0200

    Login ui design

--- src/main/java/gui/Login.java
+++ src/main/java/gui/Login.java
@@ -1,6 +1,8 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Login {
     private JPanel Login;
@@ -9,7 +11,7 @@ public class Login {
     private JLabel textPassword;
     private JTextField FieldNome;
     private JPasswordField FieldPassword;
-    private JButton button1;
+    private JButton buttonLogin;
     final int carattere = 20;
 
     private static JFrame frame;
@@ -20,6 +22,23 @@ public class Login {
         textPassword.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, carattere));
         FieldNome.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, carattere));
         FieldPassword.setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, carattere));
+
+        buttonLogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = FieldNome.getText();
+                String password = new String(FieldPassword.getPassword());
+
+                if (username.equals("a") && password.equals("b")) {
+                    JOptionPane.showMessageDialog(frame, "Login effettuato");
+                    //new Home().setVisible(true);
+                   frame.setVisible(false);
+                } else {
+                    JOptionPane.showMessageDialog(frame, "Credenziali non valide", "Errore", JOptionPane.ERROR_MESSAGE);
+                }
+
+            }
+        });
     }
 
     public static void main(String[] args) {
