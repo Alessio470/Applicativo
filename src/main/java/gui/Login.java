9919c40b772993df95d2c6efc4df5bd82b214bdc
Alessio470 <nazzaroalessio@gmail.com>
2025-09-09 15:31:37 +0200

    Riformattazione GUI

--- src/main/java/gui/Login.java
+++ src/main/java/gui/Login.java
@@ -28,7 +28,7 @@ public class Login {
     public static JFrame frame;
 
     public static void main(String[] args) {
-        frame = new JFrame("HomeCourses");
+        frame = new JFrame("Login");
         frame.setContentPane(new Login().PanelLogin);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
