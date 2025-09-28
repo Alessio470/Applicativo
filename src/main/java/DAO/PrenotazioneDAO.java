bbce00a0d47fd30159f3c89b8a0e65be97fb2b4c
M1rio2004 <132071501+M1rio2004@users.noreply.github.com>
2025-09-28 12:04:03 +0200

    aggiunta altra roba da finire

--- src/main/java/DAO/PrenotazioneDAO.java
+++ src/main/java/DAO/PrenotazioneDAO.java
@@ -15,16 +15,22 @@ public class PrenotazioneDAO {
     public PrenotazioneDAO( Connection conn ) { this.conn=conn; }
 
 
-    //TODO sta roba qua
+
     public int InserisciPrenotazione(Prenotazione prenotazione) throws SQLException {
 
-final String sql = "INSERT INTO prenotazione(numerobiglietto, numeroposto, statoprenotazione, username, codvolo, nomepasseggero, cognomepasseggero, codicefiscalepasseggero)" +
+    final String sql = "INSERT INTO prenotazione(numerobiglietto, numeroposto, statoprenotazione, username, codvolo, nomepasseggero, cognomepasseggero, codicefiscalepasseggero)" +
                 "VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
 
         try(PreparedStatement ps = conn.prepareStatement(sql)){
 
             ps.setString(1,prenotazione.getNumeroBiglietto());
-
+            ps.setString(2,prenotazione.getPosto());
+            ps.setInt(3, prenotazione.getStato().ordinal());
+            ps.setString(4,prenotazione.getUsernameUtente());
+            ps.setString(5,prenotazione.getCodiceVolo());
+            ps.setString(6,prenotazione.getNomePasseggero());
+            ps.setString(7,prenotazione.getCognomePasseggero());
+            ps.setString(8,prenotazione.getCodicefiscalepasseggero());
 
             try (ResultSet rs = ps.executeQuery()) {
                 if (rs.next()) {
