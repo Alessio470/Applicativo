d7652109a6cfd67887c178d4fcbdfaa74b506dc9
M1rio2004 <132071501+M1rio2004@users.noreply.github.com>
2026-01-17 19:19:19 +0100

    ref: rigenerato la javadocs

--- documentazione/javadoc/package-search-index.js
+++ documentazione/javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controller"},{"l":"DAO"},{"l":"database"},{"l":"gui"},{"l":"model"},{"l":"model.enums"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controller"},{"l":"dao"},{"l":"dao.implementazioniPostgresDAO"},{"l":"database"},{"l":"gui"},{"l":"model"},{"l":"model.enums"}];updateSearchResults();
